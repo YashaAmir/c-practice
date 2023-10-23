cc0ef8d55c5e5106752dd2d6a1e3d10f8ee319af
YashaAmir <148742424+YashaAmir@users.noreply.github.com>
2023-10-23 10:33:11 -0700

    Add files via upload

---
+++ or gate.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	cout<<"please enter value one\n";
+	cin>> a;
+	cout<<"please enter value two\n";
+	cin>> b;
+	if(a<b)
+	{
+		cout<<"a is less than b";
+	}
+	else if(a==b)
+	{
+		cout<<"a is equals to b";
+	}
+	
+}
