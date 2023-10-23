322ab6e4062ce5fd135766c0fc014f86d37dd923
YashaAmir <148742424+YashaAmir@users.noreply.github.com>
2023-10-23 10:34:41 -0700

    Add files via upload

---
+++ if else if.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueone;
+	int valuetwo;
+    cout<<"please enter valueone";
+	cin>> valueone;
+	cout<<"please enter valuetwo";
+	cin>> valuetwo;
+	if(valueone>valuetwo)
+	{
+		cout<<"valueone is greater than valuetwo";
+	}
+	else if(valueone==valuetwo)
+	{
+		cout<<"valueone is equals to valuetwo";
+	}
+	else
+	{
+		cout<<" valueone is less than valuetwo";
+	}
+}
+	
