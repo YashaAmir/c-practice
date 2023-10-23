26f8adf293c6c8b12de87d50cd8f4d1ceb7dab39
YashaAmir <148742424+YashaAmir@users.noreply.github.com>
2023-10-23 10:32:20 -0700

    Add files via upload

---
+++ normalgate.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char characterone;
+	char charactertwo;
+	cout<<"please enter first chatacter\n";
+	cin>> characterone;
+	cout<<"please enter second character\n ";
+	cin>> charactertwo;
+	if(characterone==charactertwo)
+	{
+		cout<<"characters are same";
+	}
+	else
+	{
+		cout<<"characters are not same";
+	}
+}
+	
+
