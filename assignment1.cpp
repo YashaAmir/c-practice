97a5459faa4a5a5b11ce5891a9049a5b9acbd05e
YashaAmir <148742424+YashaAmir@users.noreply.github.com>
2023-10-23 10:31:56 -0700

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	int b = 115;
+	int c;
+	
+	cout<<"the values before swapping are a is "<<a <<" b is "<<b<<endl;
+	c = a;
+	a=b;
+	b=c;
+	cout<<"the value after swapping are a is "<<a <<" b is "<<b<<endl;
+}
